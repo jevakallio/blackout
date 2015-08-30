b4c16ebb153267492bdfad3aecb9170a194de0cb
Jani Evakallio <jani.evakallio@gmail.com>
2015-08-30 19:52:08 +0100

    Answer input (handling wip)

--- src/components/answer-view.js
+++ src/components/answer-view.js
@@ -2,22 +2,48 @@
 
 const React = require('react');
 const {string, bool} = React.PropTypes;
+const KEY_ENTER = 13;
 
 const AnswerView = React.createClass({
   displayName: 'AnswerView',
   propTypes: {
 
     // state
+    levelQuestion: string.isRequired,
     levelAnswer: string.isRequired,
     levelAnswerIsExact: bool.isRequired
 
     //actions
   },
+
+  onKeyPress(e) {
+    if(e.which === KEY_ENTER) {
+
+    }
+  },
+
   render() {
     return (
-      <div className='answer-container'>{this.props.levelAnswer}</div>
+      <div className='answer-container'>
+        <label htmlFor='answer'>
+          {this.props.levelQuestion}
+        </label>
+        <input
+          name='answer'
+          type='text'
+          onKeyPress={this.onKeyPress}
+          style={styles.answerInput}>
+        </input>
+      </div>
     );
   }
 });
+
+const styles = {
+  answerInput: {
+    width: '95%',
+    marginRight: '5%'
+  }
+};
 
 module.exports = AnswerView;
